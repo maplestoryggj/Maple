ade353979d35f6e2cae98fae5c3cb5b861fd85b7
danilocaverna <nq3i4fsx@hotmail.com>
2019-01-26 11:34:22 -0300

    wind trail (wip)

---
+++ Projeto/Maple/Assets/Scripts/Wind.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour{
+
+    
+    private Rigidbody2D rb;
+    Camera camera;
+
+    bool isBlowing = false;
+
+    void Start(){
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update(){
+        if (Input.GetMouseButtonDown(0)){
+            BlowWind();
+        }
+        else if (Input.GetMouseButtonUp(0)){
+            StopBlowWind();
+        }
+
+        if(isBlowing){
+            updateWindTrail();
+        }
+
+    }
+    void updateWindTrail(){
+        rb.position = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public void BlowWind(){
+        isBlowing = true;
+    }
+    public void StopBlowWind(){
+        isBlowing = false;
+    }
+}
