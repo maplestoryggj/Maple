dbb965d3284c46064d1bf0ad939d86955d354c93
Leonardo Crivellaro <leonardo.lazarinocrivellaro@gmail.com>
2019-01-26 12:06:30 -0300

    FollowCamera

---
+++ Projeto/Maple/Assets/followCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+
+	public Transform target;
+
+
+
+	public float smoothSpeed = 0.125f;
+
+	public Vector3 offset;
+
+
+
+	void LateUpdate ()
+
+	{
+        
+        Vector3 desiredPosition = target.position + offset;
+        desiredPosition.z = -10;
+
+        Vector3 smoothedPosition = Vector3.Slerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+
+
+
+        //transform.LookAt(target);
+
+    }
+
+
+
+}
